028229d770732fca2483de3f33db57047b525c2b
ashishkumar9211 <ashishkumar9211@gmail.com>
2015-07-28 09:15:58 +0530

    updating git

--- src/main/java/com/java/Hello.java
+++ src/main/java/com/java/Hello.java
@@ -8,13 +8,10 @@ import javax.ws.rs.core.Response;
 @Path("/hello")
 public class Hello {
 
-    @GET
-    @Path("/{pathParameter}")
-    public Response responseMsg( @PathParam("pathParameter") String pathParameter
-            ) {
-
-        String response = "Hello from: " + pathParameter;
-
-        return Response.status(200).entity(response).build();
-    }
+	@GET
+	@Path("/{pathParameter}")
+	public Response responseMsg( @PathParam("pathParameter") String pathParameter ) {
+		String response = "Hello from: " + pathParameter;
+		return Response.status(200).entity(response).build();
+	}
 }
