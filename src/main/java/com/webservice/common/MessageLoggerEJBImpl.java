d3020e6592a3794910eb74f4ee8d41fc6ab8bb99
ashishkumar9211 <ashishkumar9211@gmail.com>
2016-04-02 10:59:31 +0530

    added margin

--- src/main/java/com/webservice/common/MessageLoggerEJBImpl.java
+++ src/main/java/com/webservice/common/MessageLoggerEJBImpl.java
@@ -10,34 +10,34 @@ import javax.sql.DataSource;
 
 public class MessageLoggerEJBImpl{
 
-	 public void saveMessageLog(){
-	       
-	        String sql     = "";
-	    try{
-		    	DataSource pool;
-			    Connection conn = null;
-			    Statement  stmt = null;	 
-			    
-		       /** Create a JNDI Initial context to be able to lookup the DataSource */
-		       InitialContext ctx = new InitialContext();
-		       /** Lookup the DataSource, which will be backed by a pool */
-		       
-		       /**   that the application server provides. */
-		       pool = (DataSource)ctx.lookup("java:jboss/datasources/webDS");
-		       if (pool == null){
-		           throw new Exception("Unknown DataSource 'jdbc/webDS'");
-		       }
-		       
-		       conn = pool.getConnection();
-		       stmt = conn.createStatement();	
-		       
-	                sql = "select * from Employee";
-	                
-	                System.out.println("email :"+sql.toString());
-	                stmt.executeQuery(sql);
-	        
-	    }catch(Exception e){
-	      e.printStackTrace();
-	    }      
-	    }
+	public void saveMessageLog(){
+
+		String sql     = "";
+		try{
+			DataSource pool;
+			Connection conn = null;
+			Statement  stmt = null;	 
+
+			/** Create a JNDI Initial context to be able to lookup the DataSource */
+			InitialContext ctx = new InitialContext();
+			/** Lookup the DataSource, which will be backed by a pool */
+
+			/**   that the application server provides. */
+			pool = (DataSource)ctx.lookup("java:jboss/datasources/webDS");
+			if (pool == null){
+				throw new Exception("Unknown DataSource 'jdbc/webDS'");
+			}
+
+			conn = pool.getConnection();
+			stmt = conn.createStatement();	
+
+			sql = "select * from Employee";
+
+			System.out.println("email :"+sql.toString());
+			stmt.executeQuery(sql);
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}      
+	}
 }
